43234adf5d1ecef034706e1b5ca93ab4ff6915c9
mana jalili <jalilimana@gmail.com>
2020-06-10 21:25:01 +0200

    finalized the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'thingss#index'
+  resources :things, only: [:new, :create, :destroy, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
